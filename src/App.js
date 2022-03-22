5d6383f8978621d1fd3126038397720b3b7c1f17
floatingnahid22 <nahid.uap.github@gmail.com>
2022-03-23 00:06:13 +0600

    lesson-13 push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
-    return 'Hello World';
+    return (
+        <div className="app">
+            <ClickCounter />
+            <HoverCounter />
+        </div>
+    );
 }
 
 export default App;
